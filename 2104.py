cf72dad60ac548ba9007c44cdfcbff4e1dd13804
Bugeon <wittybear@naver.com>
2021-04-01 00:55:45 +0900

    update: https://www.acmicpc.net/problem/2104

--- 2104.py
+++ 2104.py
@@ -8,21 +8,25 @@ def get_subMax(start, end):
   subMax = max(get_subMax(start, mid), get_subMax(mid + 1, end))
   
   left, right = mid, mid + 1
-  subSum = sum(nums[left:right])
-  min_value = min(nums[left:right])
+  subSum = nums[left] + nums[right]
+  min_value = min(nums[left], nums[right])
   subMax = max(subMax, subSum * min_value)
   
   while left > start or right < end:
-    if left > start and nums[left - 1] > min_value:
+    if left > start and (right == end or nums[left - 1] > nums[right + 1]):
       subSum += nums[left - 1]
+      min_value = min(min_value, nums[left - 1])
       left -= 1
     else:
       subSum += nums[right + 1]
+      min_value = min(min_value, nums[right + 1])
       right += 1
+    # print(nums[left:right], subMax)
     subMax = max(subMax, subSum * min_value)
+    
   return subMax
 
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
 
-get_subMax(0, n - 1)
+print(get_subMax(0, n - 1))
